8ce73c3836fccc1df4452d09f231a53caefa003d
sachin <sachin@cybrilla.com>
2020-02-20 21:03:32 +0530

    Add staging env

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,11 @@ set :rails_env, 'staging'
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
 # set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
-# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+set :shared_files, fetch(:shared_files, []).push(
+  'config/database.yml', 
+  'config/secrets.yml',
+  'config/puma.rb'
+)
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
@@ -44,7 +48,6 @@ task :remote_environment do
   invoke :'rvm:use', 'ruby-2.6.3'
 end
 
-set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'config/puma.rb']
 
 # Put any custom commands you need to run at setup
 # All paths in `shared_dirs` and `shared_paths` will be created on their own.
